86a08e0f7abf7b8d5c94fda2f60a76a08735c9aa
Aries1542 <ndejongster@gmail.com>
2025-07-21 12:42:08 -0600

    shipping label test

--- utils/authorize.js
+++ utils/authorize.js
@@ -98,4 +98,44 @@ const createPaymentPage = (lineItems, shipTo, shippingMethod, callback) => {
 	});
 }
 
-export { initAuthNet, createPaymentPage };
+const getTransactionDetails = (transactionId, callback) => {
+	var getRequest = new APIContracts.GetTransactionDetailsRequest();
+	getRequest.setMerchantAuthentication(merchantAuthenticationType);
+	getRequest.setTransId(transactionId);
+
+	console.log(JSON.stringify(getRequest.getJSON(), null, 2));
+		
+	var ctrl = new APIControllers.GetTransactionDetailsController(getRequest.getJSON());
+
+	ctrl.execute(function(){
+
+		var apiResponse = ctrl.getResponse();
+
+		if (apiResponse != null) var response = new APIContracts.GetTransactionDetailsResponse(apiResponse);
+
+		console.log(JSON.stringify(response, null, 2));
+
+		if(response != null){
+			if(response.getMessages().getResultCode() == APIContracts.MessageTypeEnum.OK){
+				console.log('Transaction Id : ' + response.getTransaction().getTransId());
+				console.log('Transaction Type : ' + response.getTransaction().getTransactionType());
+				console.log('Message Code : ' + response.getMessages().getMessage()[0].getCode());
+				console.log('Message Text : ' + response.getMessages().getMessage()[0].getText());
+			}
+			else{
+				console.log('Result Code: ' + response.getMessages().getResultCode());
+				console.log('Error Code: ' + response.getMessages().getMessage()[0].getCode());
+				console.log('Error message: ' + response.getMessages().getMessage()[0].getText());
+			}
+		}
+		else{
+			var apiError = ctrl.getError();
+			console.log(apiError);
+			console.log('Null Response.');
+		}
+		
+		callback(response);
+	});
+};
+
+export { initAuthNet, createPaymentPage, getTransactionDetails };
